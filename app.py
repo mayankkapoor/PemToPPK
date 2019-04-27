a83e6475fea2f5563b3dc45f51a4a8e33eb4c3a0
Mayank Kapoor <mayankkapoormail@gmail.com>
2019-04-27 17:47:03 +0530

    Remove ppk file write

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-from flask import Flask, request, json,Response
+from flask import Flask, request, json, Response
 import os
 app = Flask(__name__)
-cwd = os.getcwd()
 
 
 @app.route("/")
 def hello():
     return "Service health check OK!\n"
 
-@app.route('/pemtoppk',methods=["POST"])
+
+@app.route('/pemtoppk', methods=["POST"])
 def pemtoppk():
     if request.method == 'POST':
         try:
-            output_filename = request.form.get('output_filename')
             pem_key = request.form.get('pem_key')
             print("pem_key: ", pem_key)
-            ppkKey = os.system("puttygen " + pem_key + " -O private -o "+ ppkFileName)
+            pemFileName = "key.pem"
+            pemFile = open(pemFileName, "w")
+            pemFile.write(pem_key)
+            pemFile.close()
+            ppkKey = os.system("puttygen " + pemFileName + " -O private")
             print("ppkKey: ", ppkKey)
             resp = Response(ppkKey, status=200, mimetype='text/html')
             return resp
         except Exception as ex:
-            print (ex)
-            return Response(error = ex, status=400, mimetype='text/html')
+            print(ex)
+            return Response(status=500, mimetype='text/html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
